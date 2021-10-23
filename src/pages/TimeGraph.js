1785c769e75587f5949ba5a4211327f653e04b74
Gui Pereira <guilhermepdo1508@gmail.com>
2021-10-23 01:08:00 -0300

    Feita estrutura inicial

---
+++ src/pages/TimeGraph.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+function TimeGraph(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+      TimeGraph.propTypes = {
+        
+      };
+
+export default TimeGraph;
