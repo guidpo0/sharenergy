e1bd3f7912130f0d72b3e18d796a4de07e875d32
Gui Pereira <guilhermepdo1508@gmail.com>
2021-10-25 20:21:49 -0300

    Feito Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import instagramIcon from '../images/icons/instagram.png';
+import facebookIcon from '../images/icons/facebook.png';
+import linkedinIcon from '../images/icons/linkedin.png';
+import './Footer.css';
 
 function Footer() {
   return (
-    <div />
+    <footer>
+      <img
+        src={instagramIcon}
+        alt="instagram icon"
+        className="footer-icon"
+      />
+      <img
+        src={linkedinIcon}
+        alt="linkedin icon"
+        className="footer-icon"
+      />
+      <img
+        src={facebookIcon}
+        alt="facebook icon"
+        className="footer-icon"
+      />
+    </footer>
   );
 }
 
