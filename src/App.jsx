786eb8e7af290a2c72c029948ff8074b0a48c566
Gui Pereira <guilhermepdo1508@gmail.com>
2021-10-25 20:12:17 -0300

    Feito Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Routes from './routes/Routes';
 
 const App = () => (
   <>
+    <Header />
     <Routes />
+    <Footer />
   </>
 );
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import logo from '../images/logo.png';
+import './Header.css';
 
 function Header() {
   return (
-    <div />
+    <header>
+      <img
+        src={logo}
+        alt="website logo"
+        className="header-logo"
+      />
+      <h1 className="header-title">Sharenergy</h1>
+      <button type="button" className="header-navbar-button">
+        <div className="navbar-button-bar bar-1" />
+        <div className="navbar-button-bar bar-2" />
+        <div className="navbar-button-bar bar-3" />
+      </button>
+    </header>
   );
 }
 
