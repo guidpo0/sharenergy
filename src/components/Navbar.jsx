d1426fbd254e03488403896f04694ae3b9118b91
Gui Pereira <guilhermepdo1508@gmail.com>
2021-11-03 21:20:13 -0300

    Feito header com nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import './Navbar.css';
 
-function Navbar() {
+function Navbar({ links }) {
   return (
-    <div />
+    <nav>
+      <div className="topnav">
+        {
+          links.map(({ link, text }) => (
+            <Link
+              to={link}
+              className="topnav-link"
+            >
+              {text}
+            </Link>
+          ))
+        }
+      </div>
+    </nav>
   );
 }
+
+Navbar.propTypes = {
+  links: PropTypes.arrayOf(
+    PropTypes.shape({
+      link: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
 
 export default Navbar;
